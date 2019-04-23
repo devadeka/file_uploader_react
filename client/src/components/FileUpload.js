b75d2174e2776e73323812f9115f524b39998542
Devatanu Deka <deva.deka@gmail.com>
2019-04-23 16:49:34 +1000

    file selection and states set

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
@@ -6,15 +7,35 @@ const FileUpload = () => {
 
   const onChange = e => {
     setFile(e.target.files[0]);
-    setFilename(e.target.file[0].name);
+    setFilename(e.target.files[0].name);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+    } catch (err) {}
   };
 
   return (
     <React.Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='custom-file'>
-          <input type='file' className='custom-file-input' id='customFile' />
-          <label className='custom-file-label' for='customFile'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label className='custom-file-label' htmlFor='customFile'>
             {filename}
           </label>
         </div>
