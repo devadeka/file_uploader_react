c1878a32f2c0dcc76ed6c560bce9064706ae8e54
Devatanu Deka <deva.deka@gmail.com>
2019-04-23 14:19:41 +1000

    front end basic client added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => (
+  <div className='container'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> React File Uploader
+    </h4>
+  </div>
+);
+
+export default App;
